aa711652343683ae7f8cf89798963eb16c6413e7
Ananta Poudel <anantapoudel580@gmail.com>
2024-04-09 07:46:59 +0545

    Added new varibales to use for flag drawing and color and size

--- index.js
+++ index.js
@@ -4,4 +4,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("main");
   const ctx = canvas.getContext("2d");
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+  let brushSize = 5;
+  // Initial color set to pink
+  let color = "#F50057";
 });
