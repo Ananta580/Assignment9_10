42119fa656cc631ec925a53244a97665c5194806
Ananta Poudel <anantapoudel580@gmail.com>
2024-04-09 07:46:00 +0545

    Initialized JS file

---
+++ index.js
@@ -0,0 +1,7 @@
+// Student ID: C0913139
+// Student Name: Ananta Poudel
+
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("main");
+  const ctx = canvas.getContext("2d");
+});
